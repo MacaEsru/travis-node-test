835fcf49c47a8966b63daac822716457fc13d5f1
MacaEsru <163170@ids.upchiapas.edu.mx>
2019-11-18 14:15:00 -0600

    modificaciones

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-const express = require('express');
-
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('ci with travis');
-});
-
-const server = app.listen(3000, () => {
-  console.log('App running on port 3000');
-});
-
-module.exports = server;
+module.exports = {
+  add: function (a, b) {
+    return a + b;
+  }
+};
